4ab70e1cacbcf2892043ce8caba71e1a6ec3c87c
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2025-12-05 18:49:14 +0100

    feat: implement mergeObjects

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,18 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const mergeObj = {};
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      if (mergeObj[key]) {
+        mergeObj[key] += value;
+      } else {
+        mergeObj[key] = value;
+      }
+    });
+  });
+  return mergeObj;
 }
 
 /**
